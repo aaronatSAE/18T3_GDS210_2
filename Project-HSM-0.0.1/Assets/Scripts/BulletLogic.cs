02671a4f5b213a50890b3449e0be5ffbf895d3e0
Azaloft <40777990+Azaloft@users.noreply.github.com>
2018-10-23 11:42:26 +1100

    Basic player action
    wasd movement
    spawns bullet when mouse button 1 is pressed
    basic models for wall, broken wall, player and bullets

---
+++ Project-HSM-0.0.1/Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour {
+    Rigidbody rb;
+    public float bulletspeed = 300;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.forward * bulletspeed);
+	}
+}
---
+++ Project-HSM-0.0.1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    float x;
+    float z;
+    public float movespeed = 5;
+    public float turnrate;
+    public float jumpheight = 10;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * turnrate;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, x, 0);
+        if(Input.GetKeyDown("space"))
+        {
+            rb.AddForce(transform.up * jumpheight);
+        }
+        if( x == 1)
+        {
+            transform.Rotate(0, 90, 0);
+        }
+	}
+}

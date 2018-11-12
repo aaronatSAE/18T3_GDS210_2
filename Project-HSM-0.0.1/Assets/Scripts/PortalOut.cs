26bb415a985acfc6b757b28db2f377a62349bff1
Azaloft <40777990+Azaloft@users.noreply.github.com>
2018-11-12 15:56:10 +1100

    Relics and Minimap
    Add basic relic system, when player collects 3 relics the player will be teleported away
    minimap on UI

---
+++ Project-HSM-0.0.1/Assets/Scripts/PortalOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalOut : MonoBehaviour {
+    public string scene;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            print("run");
+            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+        }
+    }
+}

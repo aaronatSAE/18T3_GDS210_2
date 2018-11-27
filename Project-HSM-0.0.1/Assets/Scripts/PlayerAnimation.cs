e6b4313171462b1805d9f7bae405e72c8de40a5a
Init101 <40778008+Init101@users.noreply.github.com>
2018-11-27 17:43:54 +1100

    Animation Controller
    Idle > Running animation working
    Script Made
    Prefab Updated

---
+++ Project-HSM-0.0.1/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    //Step Variable for animator
+    private Animator animator;
+
+    //Use this for Initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else if (Input.GetKey("a"))
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else if (Input.GetKey("s"))
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else if (Input.GetKey("d"))
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);            
+        }
+    }
+}

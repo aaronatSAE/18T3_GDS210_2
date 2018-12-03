65dd1350826f38144f85b727fa6f215c33b96210
Azaloft <40777990+Azaloft@users.noreply.github.com>
2018-12-04 10:54:03 +1100

    game update
    enemy animations are now working
    relocated exit door spawn location

--- Project-HSM-0.0.1/Assets/Scripts/MeleeEnemyAttack.cs
+++ Project-HSM-0.0.1/Assets/Scripts/MeleeEnemyAttack.cs
@@ -7,12 +7,15 @@ public class MeleeEnemyAttack : MonoBehaviour
 {
     private GameObject player;
     private HealthDisplay healthDisplay;
-    [SerializeField] private float attackDelay = 2f;
-    [SerializeField] private float attackRange = 2.5f;
+    private Animator anim;
+    [SerializeField] private float attackDelay = 2.5f;
+    [SerializeField] private float attackRange = 1.4f;
     [SerializeField] private float attackDamage = 1f;
+    [SerializeField] private float animationDelay = 0.9f;
 
     // finds the player and the health display which is where the player health is stored
     void Start () {
+        anim = gameObject.GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         healthDisplay = GameObject.Find("PlayerHealth").GetComponent<HealthDisplay>();
     }
@@ -21,19 +24,23 @@ public class MeleeEnemyAttack : MonoBehaviour
 	void Update () {
         if(Vector3.Distance(player.transform.position, transform.position) <= attackRange)
         {
+            if(attackDelay <= animationDelay)
+            {
+                anim.SetTrigger("IsAttacking");
+            }
             //countdown before attacking
             attackDelay -= Time.deltaTime;
             if(attackDelay < 0)
             {
                 //once timer is over the player takes damage
                 healthDisplay.healthUIImage.fillAmount -= 0.3f;
-                attackDelay = 2f;
+                attackDelay = 1.4f;
             }
         }
         //when the player leaves the attack range of the enemy then reset attack timer
         else
         {
-            attackDelay = 2f;
+            attackDelay = 1.4f;
         }
 	}
 }

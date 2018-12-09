f2a975b2fe589fb535828dd473d424e517ea6b05
Azaloft <40777990+Azaloft@users.noreply.github.com>
2018-12-09 18:45:56 +1100

    bug fix
    Offscreen spawn bugs shouldnt happen anymroe

---
+++ Project-HSM-0.0.1/Assets/Scripts/EnemyAntiClip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAntiClip : MonoBehaviour {
+
+    [SerializeField] private GameObject centre;
+    [SerializeField] private float moveSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        centre = GameObject.FindGameObjectWithTag("Centre");
+		if(transform.position.y >= 1)
+        {
+            Vector3 temp = transform.position;
+            transform.position.Set(temp.x, 1, temp.z);
+        }
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, centre.transform.position, moveSpeed);
+    }
+}
--- Project-HSM-0.0.1/Assets/Scripts/EnemyBulletLogic.cs
+++ Project-HSM-0.0.1/Assets/Scripts/EnemyBulletLogic.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletLogic : MonoBehaviour {
+public class EnemyBulletLogic : MonoBehaviour
+{
     private Rigidbody rb;
     [SerializeField] private float bulletspeed = 300;
 
-	// finds rigidbody component attached to the gameobject
-	void Start () {
+    // finds rigidbody component attached to the gameobject
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// constantly moving the bullet forward
-	void Update () {
-        rb.AddForce(transform.forward * bulletspeed * -1);
-	}
+    }
+
+    // constantly moving the bullet forward
+    void Update()
+    {
+        rb.AddForce(transform.forward * bulletspeed);
+    }
     //destory bullet on collision
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(transform.parent.gameObject);
         Destroy(gameObject);
     }
 }
